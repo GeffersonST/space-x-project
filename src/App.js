cc63711f6affe5c6cb229d8e8baafefbdfe57b3b
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2021-07-27 23:42:17 -0300

    clean React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { fetchHistory } from "./api";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [data, setData] = useState([]);
+
+  const getHistory = async () => {
+    console.log("Getting history");
+    const history = await fetchHistory({
+      start: startDate,
+      end: endDate
+    });
+    setData(history);
+  };
+  useEffect(() => {
+    getHistory();
+  }, [startDate, endDate]);
+
+  return (
+    <div>
+      <label> Start Date </label>
+      <input type="date" onChange={(e) => setStartDate(e.target.value)} />
+      <br />
+      <label> End Date </label>
+      <input type="date" onChange={(e) => setEndDate(e.target.value)} />
+      <ul>
+        {data.map((item, idx) => {
+          return <li key={idx}>{item.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
---
+++ src/api.js
@@ -0,0 +1,23 @@
+var requestOptions = {
+  method: "GET",
+
+  redirect: "follow"
+};
+
+const baseUrl = "https://api.spacexdata.com/v3/history";
+
+export const fetchHistory = async (filters) => {
+  let url = baseUrl;
+  if (filters.start && filters.end) {
+    url += `?start=${filters.start}&end=${filters.end}`;
+  }
+  console.log(url);
+  try {
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
